67196af650220e46da4cd49104c72bbf0ed8f1a7
Pierre-Antoine HIGNARD <pa.hignard@pm.me>
2026-03-11 14:41:20 +0100

    Updated game_state.py to be more modular

--- src/core/game_state.py
+++ src/core/game_state.py
@@ -1,16 +1,17 @@
-from config.modes import MODES, LEVEL, MODE_CONFIG, SUPPORTED_LANGUAGES
+from config.modes import MODES, LEVEL, MODE_CONFIG
+from config.dictionary import DICTIONARY
 from utils.logger import get_logger
 
 class GameStateParameters:
     def __init__(self, logger):
         self.mode = MODES[0]
-        self.language = SUPPORTED_LANGUAGES[0]
+        self.language = DICTIONARY.keys[0]
         self.questions_allowed = MODE_CONFIG[self.mode][1]["questions_allowed"]
         self.target_language_only = MODE_CONFIG[self.mode][1]["target_language_only"]
 
-        self._logger = logger
+        self.logger = logger
 
-    def update(self, mode, level, language):
+    def update_parameters(self, mode, level, language):
         """
         Updates GameState.parameters attributes according to new game parameters.
 
@@ -21,32 +22,45 @@ class GameStateParameters:
         :return: A tuple (success status, error message).
         :rtype: tuple[bool, str | None]
         """
+        # Mode
         if mode not in MODES:
-            self._logger.error(f"Mode {mode} not recognised. Used default (or previous) value instead.")
+            self.logger.error(f"Mode {mode} not recognised. Used default (or previous) value instead.")
         else:
             self.mode = mode
 
+        # Difficulty level-specific parameters
         if level not in LEVEL:
-            self._logger.error(f"Level {level} not recognised. Used default (or previous) value instead.")
+            self.logger.error(f"Level {level} not recognised. Used default (or previous) value instead.")
         else:
             self.questions_allowed = MODE_CONFIG[self.mode][level]["questions_allowed"]
             self.target_language_only = MODE_CONFIG[self.mode][level]["target_language_only"]
 
-        if language not in SUPPORTED_LANGUAGES:
-            self._logger.error(f"Language {language} not recognised. Used default (or previous) value instead.")
+        # Language
+        if language not in DICTIONARY.keys:
+            self.logger.error(f"Language {language} not recognised. Used default (or previous) value instead.")
         else:
             self.language = language
 
 class GameStateCurrent:
     def __init__(self, logger):
         self.tries_history = []
-        self.number_tries = 0
+        self.response_history = []
 
         self.help_history = []
 
         self.logger = logger
 
+    def add_word_attempt(self, word, response):
+        self.tries_history.append(word)
+
+    @property
+    def tries_number(self):
+        return len(self.tries_history)
+
 class GameState:
+    """
+    Global GamsState class, holds all parameters, current_state values...
+    """
     def __init__(self):
         # Other utils
         self.logger = get_logger()
@@ -57,4 +71,24 @@ class GameState:
         # Game current state attributes
         self.current_state = GameStateCurrent(self.logger)
 
+        # Target word
+        self.target_word = str
 
+    def update_parameters(self, mode, level, language):
+        """
+        Updates GameState.parameters attributes according to new game parameters.
+
+        :param mode: Game mode ("memory", "learning", "game")
+        :param level: Game difficulty level [1, 3]
+        :param language: Game language
+
+        :return: A tuple (success status, error message).
+        :rtype: tuple[bool, str | None]
+        """
+        return self.parameters.update_parameters(mode, level, language)
+
+    def add_word_attempt(self, word, response):
+        self.current_state.add_word_attempt(word, response)
+
+    def set_target_word(self, word):
+        self.target_word = word
