93d47ea8266fc88fb9c87f67bf96c89077b682e3
AstridMarion <astridmarion63@gmail.com>
2026-03-11 10:35:30 +0100

    feat: Add dictionary file structure

---
+++ src/config/dictionary.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.corpus import wordnet #definitions, synonyms, antonyms
+
+DICTIONARY = {
+    "fr": {},
+    "en": {}
+}
+
+def get_random_word(language: str, level: int) -> str:
+    """Return a random word in the dictionary of the choosed language corresponding to the level.
+    Input:
+        - language:  language of the chosen word in ["fr", "en", "es", "de", "it", "po"]
+        - level: level of the chosen word in [1,2,3]
+    Output:
+        - word: a word
+    """
+    if language not in DICTIONARY:
+        raise ValueError(f"The selected language {language} is not supported.")
+
+    return word
+
+def word_exists(word: str, language: str) -> bool:
+    """Vérifie si un mot existe dans le dictionnaire de la langue cible."""
